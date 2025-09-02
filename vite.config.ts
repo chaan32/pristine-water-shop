04d371c46a1732451332d9618bc395b63a41275c
chaan32 <haechan8864@naver.com>
2025-09-02 22:53:03 +0900

    조금 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true,
     port: 8080,
-    proxy: {
-      // '/api'로 시작하는 모든 요청을 백엔드 서버(localhost:8080)로 전달합니다.
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true, // CORS 에러 방지를 위해 필요
-      },
-    },
   },
   plugins: [
     react(),
